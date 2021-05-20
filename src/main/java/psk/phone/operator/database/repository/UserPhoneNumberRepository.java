7e1d874a6ba9a71e0c9276dcae95fb373fc291f6
M1kkar <mikkarol39@gmail.com>
2021-05-21 00:02:02 +0200

    Dodane Invoices i InvoicesRepository

--- src/main/java/psk/phone/operator/database/repository/UserPhoneNumberRepository.java
+++ src/main/java/psk/phone/operator/database/repository/UserPhoneNumberRepository.java
@@ -2,6 +2,7 @@ package psk.phone.operator.database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import psk.phone.operator.database.entities.UserPhoneNumber;
 
 @Repository
 public interface UserPhoneNumberRepository extends JpaRepository<UserPhoneNumber, Long> {
